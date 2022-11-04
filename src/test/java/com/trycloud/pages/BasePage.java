696869345047a4758d0f843ee9d6b580eec29b3c
Alen <almendovic@gmail.com>
2022-11-04 17:19:36 -0600

    Alen created framework

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -3,7 +3,7 @@ package com.trycloud.pages;
 import com.trycloud.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage {
+public class BasePage {
     public BasePage() {
       PageFactory.initElements(Driver.getDriver(),this);
       //ba blac
