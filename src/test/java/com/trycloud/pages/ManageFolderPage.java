7000484a0a03bfb03f0fa25e6517e912a1c9c096
Alen <almendovic@gmail.com>
2022-11-04 19:59:44 -0600

    US ac1 done

---
+++ src/test/java/com/trycloud/pages/ManageFolderPage.java
@@ -0,0 +1,39 @@
+package com.trycloud.pages;
+
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ManageFolderPage {
+
+
+    public ManageFolderPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+
+    }
+
+    @FindBy(xpath = "//*[@id=\"appmenu\"]/li[2]/a")
+    public WebElement filesTab;
+
+    @FindBy(xpath = "//span[@class='icon icon-add']")
+    public WebElement addIcon;
+
+    @FindBy(xpath = "//span[.='New folder']")
+    public WebElement newFolder;
+
+    @FindBy(xpath = "//*[@id=\"view13-input-folder\"]")
+    public WebElement inputBox;
+
+    @FindBy(xpath = "//*[@id=\"controls\"]/div[2]/div[2]/ul/li[2]/a/form/input[2]")
+    public WebElement submitBtn;
+
+    @FindBy(xpath = "(//span[contains(normalize-space(),'new folder')])[2]")
+    public WebElement newfolder;
+
+
+
+
+}
---
+++ src/test/java/com/trycloud/step_definitions/manage_folders_step_Definitions_US7.java
@@ -0,0 +1,94 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.BasePage;
+import com.trycloud.pages.ManageFolderPage;
+import com.trycloud.utilities.ConfigurationReader;
+import com.trycloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class manage_folders_step_Definitions_US7 {
+
+    BasePage basePage = new BasePage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+
+    ManageFolderPage manageFolderPage =new ManageFolderPage();
+
+    Actions actions=new Actions(Driver.getDriver());
+
+
+
+
+
+    @Given("user on the dashboard page")
+    public void user_on_the_dashboard_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("trycloudUrl"));
+        wait.until(ExpectedConditions.visibilityOf(basePage.userName));
+      basePage.userName.sendKeys("User21");
+      basePage.passWord.sendKeys("Userpass123");
+      basePage.loginPage.click();
+
+    }
+    @When("the user clicks the {string} module")
+    public void the_user_clicks_the_module(String string) {
+        wait.until(ExpectedConditions.visibilityOf(manageFolderPage.filesTab));
+       // actions.clickAndHold(manageFolderPage.filesTab).perform();
+
+        manageFolderPage.filesTab.click();
+
+
+    }
+    @When("user clicks the add icon on the top")
+    public void user_clicks_the_add_icon_on_the_top() {
+        manageFolderPage.addIcon.click();
+
+    }
+    @When("user click \"new folder”")
+    public void user_click_new_folder() {
+        manageFolderPage.newFolder.click();
+
+    }
+    @When("user write a folder name")
+    public void user_write_a_folder_name() {
+        wait.until(ExpectedConditions.visibilityOf(manageFolderPage.inputBox));
+      manageFolderPage.inputBox.clear();
+      manageFolderPage.inputBox.sendKeys("new folder");
+    }
+    @When("the user click submit icon")
+    public void the_user_click_submit_icon() {
+        wait.until(ExpectedConditions.visibilityOf(manageFolderPage.submitBtn));
+        manageFolderPage.submitBtn.click();
+
+    }
+    @Then("Verify the folder is displayed on the page")
+    public void verify_the_folder_is_displayed_on_the_page() {
+        wait.until(ExpectedConditions.visibilityOf(manageFolderPage.newfolder));
+        String expectecFolderName="new folder";
+        String actualFolderName=manageFolderPage.newfolder.getText();
+
+        Assert.assertEquals(expectecFolderName,actualFolderName);
+        System.out.println("Title Verified: = "+manageFolderPage.newfolder.getText());
+
+    }
+
+
+    @And("user choose a folder from the page")
+    public void userChooseAFolderFromThePage() {
+        
+    }
+
+    @When("the user uploads a file with the upload file option")
+    public void theUserUploadsAFileWithTheUploadFileOption() {
+        
+    }
+
+    @Then("Verify the file is displayed on the page")
+    public void verifyTheFileIsDisplayedOnThePage() {
+    }
+}
