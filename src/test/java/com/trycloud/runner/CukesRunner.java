06a427718907f4571861889083f9d17e9e1791d9
Alen <almendovic@gmail.com>
2022-11-06 13:20:22 -0700

    ALen made changes

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "",
         publish = true
 
 )
